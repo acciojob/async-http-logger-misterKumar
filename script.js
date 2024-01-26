078e44a9ea5df6c0bf7900d2aca970e3e384c7d7
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-26 11:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function httpLogger(){
+fetch("https://jsonplaceholder.typicode.com/todos/1").then(response => response.json()).then(data => console.log("API Response:", data)).catch(error => console.error("Error:", error));
+}
+httpLogger();
